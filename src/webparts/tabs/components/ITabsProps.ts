8a668ece29dc806a8e67d6955862ddfcda0d41e3
Michal Adam <michalaizdebski@outlook.com>
2018-09-10 14:37:31 +0200

    webpart

---
+++ src/webparts/tabs/components/ITabsProps.ts
@@ -0,0 +1,3 @@
+export interface ITabsProps {
+  description: string;
+}
---
+++ src/webparts/tabs/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITabsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TabsWebPartStrings' {
+  const strings: ITabsWebPartStrings;
+  export = strings;
+}
